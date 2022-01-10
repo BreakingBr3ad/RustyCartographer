6fb9ba9f585479bb9b26ee8c8720744c56eb0af5
Bread <21990366+BreakingBr3ad@users.noreply.github.com>
2022-01-09 22:58:54 -0700

    Update monumentsDict.py

--- monumentsDict.py
+++ monumentsDict.py
@@ -19,26 +19,5 @@ dict = {
 'Sphere_Tank' : '❌',
 'Trainyard' : '❌',
 'Water_Treatment' : '❌'}
-    
-    #embed.add_field(name="Airfield", value = monumentsEmbed["Airfield"], inline = True)
-    #embed.add_field(name="Bandit Town", value = monumentsEmbed["Bandit_Town"], inline = True)
-    #embed.add_field(name="Excavator", value = monumentsEmbed["Excavator"], inline = True)
-    #embed.add_field(name="Harbor Large", value = monumentsEmbed["Harbor_Large"], inline = True)
-    #embed.add_field(name="Harbor Small", value = monumentsEmbed["Harbor_Small"], inline = True)
-    #embed.add_field(name="Junkyard", value = monumentsEmbed["Junkyard"], inline = True)
-    #embed.add_field(name="Launch Site", value = monumentsEmbed["Launch_Site"], inline = True)
-    #embed.add_field(name="Lighthouse", value = monumentsEmbed["Lighthouse"], inline = True)
-    #embed.add_field(name="Military Base A", value = monumentsEmbed["Military_Base_A"], inline = True)
-    #embed.add_field(name="Military Base B", value = monumentsEmbed["Military_Base_B"], inline = True)
-    #embed.add_field(name="Military Base C", value = monumentsEmbed["Military_Base_C"], inline = True)
-    #embed.add_field(name="Military Base D", value = monumentsEmbed["Military_Base_D"], inline = True)
-    #embed.add_field(name="Military Tunnels", value = monumentsEmbed["Military_Tunnels"], inline = True)
-    #embed.add_field(name="Outpost", value = monumentsEmbed["Outpost"], inline = True)
-    #embed.add_field(name="Powerplant", value = monumentsEmbed["Powerplant"], inline = True)
-    #embed.add_field(name="Satellite Dish", value = monumentsEmbed["Satellite_Dish"], inline = True)
-    #embed.add_field(name="Sewer Branch", value = monumentsEmbed["Sewer_Branch"], inline = True)
-    #embed.add_field(name="Sphere Tank", value = monumentsEmbed["Sphere_Tank"], inline = True)
-    #embed.add_field(name="Trainyard", value = monumentsEmbed["Trainyard"], inline = True)
-    #embed.add_field(name="Water Treatment", value = monumentsEmbed["Water_Treatment"], inline = False)
 
 monuments = ["Airfield","Bandit_Town","Excavator","Harbor_Large","Harbor_Small","Junkyard","Launch_Site","Lighthouse","Military_Base_A","Military_Base_B","Military_Base_C","Military_Base_D","Military_Tunnels","Outpost","Powerplant","Satellite_Dish","Sewer_Branch", "Sphere_Tank", "Trainyard", "Water_Treatment"]
